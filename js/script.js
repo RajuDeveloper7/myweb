d0cce76b167d98abe942c2bc44169777c57b0b6a
Rajutmax <rajubca20@gmail.com>
2023-04-08 16:51:23 +0530

    third commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
-window.onload = () => {
-    alert("welcome")
+function downloadFile() {
+    var fileURL = "./documents/Raju_Resume.pdf";
+    var fileName = "Raju_Resume.pdf";
+    var link = document.createElement("a");
+    link.href = fileURL;
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
 }
